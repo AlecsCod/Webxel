eceaa401716193591ebb9070d876d9a09b26078e
Alex <131803556+Tricodzy@users.noreply.github.com>
2023-05-15 21:18:36 +0300

    hhh

--- proiect/game.js
+++ proiect/game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 160;
+canvas.width = 240;
 canvas.height = 160;
 
 const keys = [];
@@ -13,62 +13,84 @@ const player = {
     height: 16,
     frameX: 0,
     frameY: 0,
-    speed: 8,
+    speed: 1,
     moving: false
+};
+
+const box = {
+    x: 32,
+    y: 32,
+    width: 16,
+    height: 16,
+    frameX: 0,
+    frameY: 0,
 };
 
 const playerSprite = new Image();
 playerSprite.src = "images/gameAssets/character.png";
 const background = new Image();
-background.src = "images/gameAssets/tiles.png";
+background.src = "images/gameAssets/metal_plate.png";
+const boxSprite = new Image();
+boxSprite.src = "images/gameAssets/box.png";
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 
-var currentKey;
 
-window.addEventListener("keydown", (keyyy) => {
-    currentKey = keyyy.key;
-    player.moving = true;
-    console.log(currentKey);
+window.addEventListener("keydown", function (keyEvent) {
+    keys[keyEvent.keyCode] = true;
+    if (keyEvent.keyCode >= 39 && keyEvent.keyCode <= 40) player.moving = true;
 });
-window.addEventListener("keyup", (keyyy) => {
-    currentKey = null;
+
+window.addEventListener("keyup", function(keyEvent) {
+    delete keys[keyEvent.keyCode];
     player.moving = false;
 });
 
 function movePlayer()
 {
-    switch(currentKey)
+    if (keys[38] && player.y > 0)
     {
-        case "w":
-            if (player.y > 100)
-            {
-                player.y -= player.speed;
-                player.frameY = 3;
-                player.moving = true;
-            }
-        case "a":
-            if (player.x > 0)
-            {
-                player.x -= player.speed;
-                player.frameY = 1;
-                player.moving = true;
-            }
-        case "s":
-            if (player.y < canvas.height - player.height) {
-                player.y += player.speed;
-                player.frameY = 0;
-                player.moving = true;
-            }
-        case "d":
-            if (player.x < canvas.width - player.width)
-            {
-                player.x += player.speed;
-                player.frameY = 2;
-                player.moving = true;
-            }
+        player.y -= player.speed;
+        //player.frameY = 3;
+        player.moving = true;
+    }
+    if (keys[37] && player.x > 0)
+    {
+        player.x -= player.speed;
+        player.frameY = 1;
+        player.moving = true;
+    }
+    if (keys[40] && player.y < canvas.height - player.height) {
+        player.y += player.speed;
+        //player.frameY = 0;
+        player.moving = true;
+    }
+    if (keys[39] && player.x < canvas.width - player.width)
+    {
+        player.x += player.speed;
+        player.frameY = 2;
+        player.moving = true;
+    }
+}
+
+function moveBox()
+{
+    if (keys[38] && box.y + 16 == player.y && player.x + 16 >= box.x && box.x + 16 > player.x && box.y > 16)
+    {
+        box.y -= player.speed;
+    }
+    if (keys[37] && box.x + 16 == player.x && player.y + 16 >= box.y && box.y + 15 > player.y && box.x > 16)
+    {
+        box.x -= player.speed;
+    }
+    if (keys[40] && player.y + 16 == box.y && box.x + 16 >= player.x && player.x + 15 > box.x && box.y < canvas.height - box.height - 16) {
+        box.y += player.speed;
+    }
+    if (keys[39] && player.x + 16 == box.x && box.y + 16 >= player.y && player.y + 15 > box.y && box.x < canvas.width - box.width - 16)
+    {
+        box.x += player.speed;
     }
 }
 
@@ -77,7 +99,7 @@ function handlePlayerFrame() {
     else player.frameX = 0;
 }
 
-let fps, fpsInterval, startTime, now, then, elapsed;
+let fps, fpsInterval, startTime, now, then, elapsed, sinceStart = 0;
 
 function startAnimating(fps){
     fpsInterval = 1000/fps;
@@ -87,11 +109,12 @@ function startAnimating(fps){
 }
 
 function animate(){
-    requestAnimationFrame(animate)
     now = Date.now();
     elapsed = now - then;
-    movePlayer();
+    requestAnimationFrame(animate);
+    
     if (elapsed > fpsInterval){
+        sinceStart++;
         then = now - (elapsed % fpsInterval);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -101,8 +124,13 @@ function animate(){
         ctx.fill();
 
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
-        
-        handlePlayerFrame();
+        drawSprite(boxSprite, box.width * box.frameX, box.height * box.frameY, box.width, box.height, box.x, box.y, box.width, box.height);
+        movePlayer();
+        moveBox();
+
+        if (sinceStart / 2 % 4 == 0){
+            handlePlayerFrame();
+        }
     }
 }
 startAnimating(60);
